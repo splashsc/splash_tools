32c6cae935fd8e915b671921e950d859db5499b0
splashsc <1844113527@qq.com>
2023-06-21 22:38:00 +0800

    22

--- find/1.py
+++ find/1.py
@@ -1,42 +1,28 @@
 import os
-import imghdr
 
-img_file = ["jpeg","png","gif","tiff","rgb","pbm","pgm","ppm","rast","xbm","bmp","webp","exr"]
+elf_files = []
+non_elf_files = []
 
 def traverse_directory(directory):
-    file_list = []
     for root, dirs, files in os.walk(directory):
         for file in files:
-            file_list.append(os.path.join(root, file))
-    return file_list
+            file_path = os.path.join(root, file)
+            if os.path.isfile(file_path):
+                if os.access(file_path, os.X_OK):
+                    with open(file_path, 'rb') as f:
+                        if f.read(4) == b'\x7fELF':
+                            elf_files.append(file_path)
+                        else:
+                            non_elf_files.append(file_path)
 
 
+traverse_directory("/home/iot/Desktop/_ax3004i_ml_14_162.bin.extracted/squashfs-root")
 
+print("ELF files:")
+for file in elf_files:
+    print(file)
 
-directory = "/home/iot/Desktop/_ax3004i_ml_14_162.bin.extracted/squashfs-root"
-file_list = traverse_directory(directory)
-# print(file_list)
+print("\nNon-ELF files:")
+for file in non_elf_files:
+    print(file)
 
-def is_binary_file(file_path):
-    try:
-        print(type(imghdr.what(file_path)))
-        if imghdr.what(file_path) in img_file:
-            return False
-        else:
-            with open(file_path, 'rb') as f:
-                data = f.read(8000)
-                if b'\0' in data:
-                    return True
-                else:
-                    return False
-    except OSError as err:
-        print(err)
-        return False
-
-binary_files = []
-for file_path in file_list:
-    if is_binary_file(file_path):
-        binary_files.append(file_path)
-
-for file_path in binary_files:
-    print(file_path)
